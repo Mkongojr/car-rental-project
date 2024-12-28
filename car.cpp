6f8719988ecfafd05fb9106fc10c220d666c884b
mkongojr <mmkongo22@gmail.com>
2024-12-28 12:23:43 +0300

    declaring inputs

---
+++ car.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;
+
+int reg[5] = {100, 101, 102, 103, 104};
+string comp[5] = {"suzuki", "crown", "toyota", "canter", "subaru"};
+int rent[5] = {2000, 2500, 3000, 3500, 4000};
+char available[5] = {'y', 'y', 'y', 'y', 'y'};

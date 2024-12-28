8ed1bcfa79d790a7af3edc1dbfe8e8dc0b7d3a62
mkongojr <mmkongo22@gmail.com>
2024-12-28 12:42:29 +0300

    user interaction

--- car.cpp
+++ car.cpp
@@ -5,3 +5,17 @@ int reg[5] = {100, 101, 102, 103, 104};
 string comp[5] = {"suzuki", "crown", "toyota", "canter", "subaru"};
 int rent[5] = {2000, 2500, 3000, 3500, 4000};
 char available[5] = {'y', 'y', 'y', 'y', 'y'};
+
+int main(){
+    while(true){
+        cout << "car rental management" << endl;
+        cout << "............." << endl;
+        cout << "1.Show available cars" << endl;
+        cout << "2.reserve a car." << endl;
+        cout << "3.Return a car." << endl;
+        cout << "4.Exit." << endl;
+        int choice;
+        cout << "enter your choice" << endl;
+        cin >> choice;
+         }
+}
